72cf6f320bc04ee70b3dcd5af59e4320fe0db665
jondavidn <Jim@jims-imac-3.local>
2016-06-04 00:01:14 -0500

    Initial Commit

---
+++ CrimeMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrimeMap
+//
+//  Created by Jim on 6/4/16.
+//  Copyright © 2016 Jonathan Niemerg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
